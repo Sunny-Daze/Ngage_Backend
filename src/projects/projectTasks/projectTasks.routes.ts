d53fd6525b82d6181718e771f6e2493150d91320
abhinavyadav <yadavabhinavkumar@gmail.com>
2023-04-13 16:06:35 +0530

    working on Projects

--- src/projects/projectTasks/projectTasks.routes.ts
+++ src/projects/projectTasks/projectTasks.routes.ts
@@ -1,17 +1,17 @@
-import express, { Router } from "express";
-import { verifyJwtToken } from "../../utils/middleware/JWT";
-import {
-  createTrainingTask,
-  updateTrainingTask,
-  deleteTrainingTask,
-  completeTraningTask
-} from "./projectTasks.controller";
-export const TraningTaskRoutes: Router = express.Router();
+// import express, { Router } from "express";
+// import { verifyJwtToken } from "../../utils/middleware/JWT";
+// import {
+//   createTrainingTask,
+//   updateTrainingTask,
+//   deleteTrainingTask,
+//   completeTraningTask
+// } from "./projectTasks.controller";
+// export const TraningTaskRoutes: Router = express.Router();
 
-TraningTaskRoutes.post("/createTrainingTask", verifyJwtToken, createTrainingTask);
-TraningTaskRoutes.post("/updateTrainingTask", verifyJwtToken, updateTrainingTask);
-TraningTaskRoutes.post("/deleteTrainingTask", verifyJwtToken, deleteTrainingTask);
-TraningTaskRoutes.post("/completeTraningTask", verifyJwtToken, completeTraningTask);
+// TraningTaskRoutes.post("/createTrainingTask", verifyJwtToken, createTrainingTask);
+// TraningTaskRoutes.post("/updateTrainingTask", verifyJwtToken, updateTrainingTask);
+// TraningTaskRoutes.post("/deleteTrainingTask", verifyJwtToken, deleteTrainingTask);
+// TraningTaskRoutes.post("/completeTraningTask", verifyJwtToken, completeTraningTask);
 
 
 
